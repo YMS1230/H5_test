e69e396e9c9cd145f38b54f8786d18fca9076255
杨茂盛 <>
2018-12-12 15:13:42 +0800

    提交

---
+++ untitled24/demo.js
@@ -0,0 +1,15 @@
+/**
+ * round函数 
+ * */
+function caculatorRound(x){
+    var tmp = Math.round(x);
+    console("四舍五入后的结果:"+tmp);
+}
+
+function demo1(){
+    var  yms = new Array(3);
+    yms[0] = "yms";
+    yms[1] = "sex";
+    yms[2] = "18";
+    document.write(yms.join("."));
+}
